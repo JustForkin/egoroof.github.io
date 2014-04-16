da8dc1c236053cd1f492a56f3f425a3035203714
Tema <unlucky@inbox.ru>
2014-04-16 22:02:55 +0900

    папой!

---
+++ js/config.js
@@ -0,0 +1,49 @@
+var images = {
+    unicorn_right: 'img/unicorn_right.png',
+    unicorn_left: 'img/unicorn_left.png',
+    grass: 'img/grass_green.png',
+    cloud_0: 'img/cloud_blue.png',
+    cloud_1: 'img/cloud_brown.png',
+    cloud_2: 'img/cloud_green.png',
+    cloud_3: 'img/cloud_pink.png',
+    cloud_4: 'img/cloud_red.png'
+};
+var cloudsPositions = [0, 185, 370, 555, 740];
+var unicorns = [
+    {
+        speedX: 1,
+        speedY: 2,
+        x: 1,
+        y: 1
+    },
+    {
+        speedX: 3,
+        speedY: 2,
+        x: 150,
+        y: 50
+    },
+    {
+        speedX: 1.5,
+        speedY: 3,
+        x: 100,
+        y: 20
+    },
+    {
+        speedX: 1,
+        speedY: 0,
+        x: 500,
+        y: 270
+    },
+    {
+        speedX: 1,
+        speedY: 0,
+        x: 400,
+        y: 280
+    },
+    {
+        speedX: 1,
+        speedY: 0,
+        x: 300,
+        y: 290
+    }
+];
---
+++ js/core.js
@@ -0,0 +1,88 @@
+var context, canvas, isSpacePressed = false;
+function init() {
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 400;
+    debug = document.getElementsByClassName('debug')[0];
+    document.addEventListener('keypress', function(e) {
+        if (e.keyCode === 32) {
+            isSpacePressed = !isSpacePressed;
+        }
+    });
+    load(function() {
+        setInterval(display, 10);
+    });
+}
+
+function load(callback) {
+    var imagesCount = Object.keys(images).length;
+    context.font = 'italic 30px Arial';
+    context.fillText('Загрузка: 0 из ' + imagesCount,
+            canvas.width / 2 - 100, canvas.height / 2);
+    var successLoads = 0;
+    for (var name in images) {
+        var path = images[name];
+        images[name] = new Image();
+        images[name].onload = function() {
+            successLoads++;
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.fillText('Загрузка: ' + successLoads + ' из ' + imagesCount,
+                    canvas.width / 2 - 100, canvas.height / 2);
+            if (successLoads === imagesCount) {
+                callback();
+            }
+        };
+        images[name].onerror = function() {
+            console.error('Не удалось загрузить: ' + this.src);
+        };
+        images[name].src = path;
+    }
+}
+
+function display() {
+    if (!isSpacePressed) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawGrass();
+        drawClouds();
+        drawUnicorns();
+    }
+}
+
+function drawGrass() {
+    for (var x = 8; x < canvas.width; x += 40) {
+        for (var y = 310; y < canvas.height; y += 30) {
+            context.drawImage(images['grass'], x, y);
+        }
+    }
+}
+
+function drawClouds() {
+    for (var i = 0; i < 5; i++) {
+        if (cloudsPositions[i] >= canvas.width) {
+            cloudsPositions[i] = -images['cloud_' + i].width;
+        }
+        context.drawImage(images['cloud_' + i], cloudsPositions[i], 0);
+        cloudsPositions[i] += 1.5;
+    }
+}
+
+function drawUnicorns() {
+    for (var i in unicorns) {
+        if (unicorns[i]['y'] + images['unicorn_right'].height >= canvas.height
+                || unicorns[i]['y'] <= 0) {
+            unicorns[i]['speedY'] *= -1;
+        }
+        if (unicorns[i]['x'] + images['unicorn_right'].width >= canvas.width
+                || unicorns[i]['x'] <= 0) {
+            unicorns[i]['speedX'] *= -1;
+        }
+        unicorns[i]['x'] += unicorns[i]['speedX'];
+        unicorns[i]['y'] += unicorns[i]['speedY'];
+        if (unicorns[i]['speedX'] > 0) {
+            context.drawImage(images['unicorn_right'], unicorns[i]['x'], unicorns[i]['y']);
+        } else {
+            context.drawImage(images['unicorn_left'], unicorns[i]['x'], unicorns[i]['y']);
+        }
+    }
+}
