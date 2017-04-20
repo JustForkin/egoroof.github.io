43655d9758574c474e0c7da6a4f7f7f1adbafe1f
Artyom Egorov <mail@egoroof.ru>
2017-04-21 00:04:51 +0700

    id3: improve UX

--- browser-id3-writer/js/stream_demo.js
+++ browser-id3-writer/js/stream_demo.js
@@ -11,20 +11,31 @@ id3Writer.setFrame('TIT2', 'Home')
 id3Writer.addTag();
 const id3Tag = new Uint8Array(id3Writer.arrayBuffer);
 
-document.getElementById('start').addEventListener('click', function () {
+const button = document.getElementById('start');
+
+button.addEventListener('click', function () {
+    button.disabled = 'disabled';
+    button.innerText = 'Fetching...';
     fetch(url).then(response => {
         const fileStream = streamSaver.createWriteStream('daft_punk_get_lucky.mp3', songSize + id3Tag.byteLength);
         const writer = fileStream.getWriter();
         const reader = response.body.getReader();
+        let readByteCount = 0;
 
         // push id3 tag first
-        writer.write(id3Tag).then(pump).then(() => console.log('All done'));
+        writer.write(id3Tag).then(pump).then(() => {
+            console.log('All done');
+            button.innerText = 'Start download';
+            button.disabled = '';
+        });
 
         function pump() {
             return reader.read().then(result => {
                 if (result.done) {
                     return writer.close();
                 } else {
+                    readByteCount += result.value.length;
+                    button.innerText = `Downloaded ${Math.ceil(readByteCount / songSize * 100)} %`;
                     return writer.write(result.value).then(pump);
                 }
             });
